4fff38259ed90f9adf5f65d262c16b51b4f49630
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-08 19:01:22 +0200

    edited navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root "static_pages#home"
 
   devise_for :users
+
+  resource :user do
+    resources :records
+  end
+  
   resources :activities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
