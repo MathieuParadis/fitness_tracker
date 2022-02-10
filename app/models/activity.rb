4dc06e40115051253b19d3ee72d705c3ec5fda0c
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-10 13:30:49 +0200

    seeded db

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,4 @@
 class Activity < ApplicationRecord
-  has_one_attached :picture
+  # has_one_attached :picture
   has_many :records
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,29 +15,52 @@ Activity.destroy_all
 p "Destroying Records... #{Record.count} "
 Record.destroy_all
 
+
+############################################
+
+p 'USERS'
+p User.create(email: "mathieu.thp@yopmail.com", password: "azerty123", first_name: "Mathieu", last_name: "Paradis")
+
 p "-" * 50
+
+p 'ACTIVITIES'
+p Activity.create(name: "Walking")
+p Activity.create(name: "Running")
+p Activity.create(name: "Cycling")
+p Activity.create(name: "Swimming")
+p Activity.create(name: "Hiking")
+p Activity.create(name: "Boxing")
+p Activity.create(name: "Taekwondo")
+p Activity.create(name: "Fencing")
+p Activity.create(name: "Golfing")
+p Activity.create(name: "Tennis")
+p Activity.create(name: "Badminton")
+p Activity.create(name: "Skateboarding")
+
 p "-" * 50
 
+p 'RECORDS'
+mathieu = User.find_by(first_name: "Mathieu", last_name: "Paradis")
+walking = Activity.find_by(name: "Walking")
+running = Activity.find_by(name: "Running")
+cycling = Activity.find_by(name: "Cycling")
+swimming = Activity.find_by(name: "Swimming")
+hiking = Activity.find_by(name: "Hiking")
+boxing = Activity.find_by(name: "Boxing")
+taekwondo = Activity.find_by(name: "Taekwondo")
+fencing = Activity.find_by(name: "Fencing")
+tennis = Activity.find_by(name: "Tennis")
+badminton = Activity.find_by(name: "Badminton")
+skateboarding = Activity.find_by(name: "Skateboarding")
+
+p Record.create(user: mathieu, activity: badminton, duration: 60, date: "2021-12-08")
+p Record.create(user: mathieu, activity: walking, duration: 40, date: "2022-01-02")
+p Record.create(user: mathieu, activity: running, duration: 120, date: "2022-01-22")
+
+
+
 ############################################
-users_number = 5
-users_count = 0
-activities_number = 10
-activities_count = 0
-records_number = 50
-records_count = 0
-############################################
 
-
-
-
-
-
-
-
-
-
-############################################
-
-puts ""
-puts "Enjoy 🥃 !!!"
-puts "----------->"
+system("clear")
+p "Enjoy 🥃 !!!"
+p "----------->"
