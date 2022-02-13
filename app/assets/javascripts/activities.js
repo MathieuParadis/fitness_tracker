fd4742d240b9eb833f554c400a5a86e4277d925f
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-13 19:56:15 +0200

    edited js

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,17 +1,18 @@
-var activityCards = document.querySelectorAll('.activity-card');
+document.addEventListener("DOMContentLoaded", function(){
+  var activityCards = document.querySelectorAll('.activity-card');
 
 
-
-activityCards.forEach((card)=>{
-  card.addEventListener('mouseover', ()=>{
-    card.children[0].classList.toggle('d-none');  
-    card.children[1].classList.toggle('d-none');  
-  })
-});
-
-activityCards.forEach((card)=>{
-  card.addEventListener('mouseout', ()=>{
-    card.children[0].classList.toggle('d-none');  
-    card.children[1].classList.toggle('d-none');  
-  })
+  activityCards.forEach((card)=>{
+    card.addEventListener('mouseover', ()=>{
+      card.children[0].classList.toggle('d-none');  
+      card.children[1].classList.toggle('d-none');  
+    })
+  });
+  
+  activityCards.forEach((card)=>{
+    card.addEventListener('mouseout', ()=>{
+      card.children[0].classList.toggle('d-none');  
+      card.children[1].classList.toggle('d-none');  
+    })
+  });
 });
