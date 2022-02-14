0997140f460ab01a3fd30fb2e2f6cbf14fb6a52b
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-14 12:01:29 +0200

    edited user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,22 @@
 module UsersHelper
+  def userRecords()
+    user_records = Record.where(user_id: current_user.id)
+
+    if user_records 
+      return user_records
+    end
+
+    return false
+  end
+
+  def totalDurationTrained()
+    total_duration = 02
+    userRecords().map{|record| total_duration += record.duration}
+
+    return total_duration
+  end
+
+
   def lastRecord()
     user_records = Record.where(user_id: current_user.id)
     sorted_user_records = user_records.sort{ |a, b| b.date <=> a.date }
