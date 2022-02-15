8bc9c40eb158bcda244fccdf32fbda58f83b4968
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-15 23:48:27 +0200

    edited new and edit views for records

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,9 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var duration_input = document.querySelector('.duration-field');
-  var time = document.querySelector('.time');
-  time.innerText = duration_input.value;
+  let duration_input = document.querySelector('.duration-field');
+  let time = document.querySelector('.time');
+
+  hours = Math.floor(duration_input.value / 60);
+  let minutes = duration_input.value % 60;
+  let result = '';
+
+  hours = Math.floor(duration_input.value / 60);
+  minutes = duration_input.value % 60;
   
+  if (hours == 0 && minutes > 1) {
+    result = minutes + ' minutes' ;
+  } else if (hours == 1 && minutes > 1) {
+    result = hours + ' hour ' + minutes + ' minutes' ;
+  } else if (hours == 1 && minutes == 0) {
+    result = hours + ' hour' ;
+  } else if (hours > 1 && minutes == 0) {
+    result = hours + ' hours' ;
+  } else if (hours > 1 && minutes > 1) {
+    result = hours + ' hours ' + minutes + ' minutes' ;
+  } else if (hours == 0 && minutes == 1) {
+    result = minutes + ' minute' ;
+  } 
+
+  time.innerText = result ;
+
   duration_input.addEventListener('input', ()=>{
-    time.innerText = duration_input.value;
+    time.innerText = result ;
+    return time.innerText
   })
 });
