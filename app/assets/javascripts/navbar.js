b6ee5d724261434aa2cf6e102abca9f6dc8149fa
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-16 23:23:17 +0200

    edited js

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(){
-  var navbarBrandElements = document.querySelector('.navbar-brand');
-
-  navbarBrandElements.addEventListener('onclick', ()=>{
-    window.location.reload();
-  });
-});
+history.scrollRestoration = 'manual';
