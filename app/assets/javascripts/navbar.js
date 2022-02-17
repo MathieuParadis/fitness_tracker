defc5ace2847284a9cbe6f4e9687303a0e982aff
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-17 22:58:27 +0200

    edited homepage

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,1 +1,12 @@
 history.scrollRestoration = 'manual';
+
+// hamburgerBtn = document.querySelector("#hamburger-btn");
+// menu = document.querySelector("#navbarSupportedContent");
+
+// hamburgerBtn.addEventListener('click', ()=>{
+//   hamburgerBtn.classList.toggle('collapsed');  
+//   hamburgerBtn.getAttribute("aria-expanded") = true;
+//   menu.classList.toggle('show');  
+// });
+
+
