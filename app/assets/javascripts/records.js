8652e5819a69e8729ef08305efef14473e2fa5b8
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-17 17:01:38 +0200

    edited js files

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -30,3 +30,27 @@ document.addEventListener("DOMContentLoaded", function(){
     return time.innerText
   })
 });
+
+
+
+var flashSection = document.getElementById('flash-section');
+var flashMsg = document.getElementById('flash-message');
+var yield = document.getElementById('yield');
+
+const displayFlashDeleteRecord = () => {
+  yield.classList.add("mt-3");
+  flashSection.classList.add("alert", "alert-success");
+  flashMsg.innerHTML = "Record was successfully destroyed";
+}
+
+const removeFlashDeleteRecord  = () => {
+  yield.classList.remove("mt-3");
+  flashSection.classList.remove("alert", "alert-success");
+  flashMsg.innerHTML = "";
+}
+
+const flashDeleteRecord = () => {
+  $.when(displayFlashDeleteRecord ()).then(function(){
+    setTimeout(removeFlashDeleteRecord , 1500);
+  })
+}
