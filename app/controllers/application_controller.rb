87b081844289b06c58a830d674f8b268741b9e83
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-18 20:29:55 +0200

    edited application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+
+  def after_inactive_sign_up_path_for(user)
+      flash[:notice] = 'You successfully signed up'
+
+    # if user_signed_in?
+    #   flash[:notice] = 'You successfully signed up'
+    #   activities_path
+    # else
+    #   super
+    # end
+  end
+
+  def after_sign_in_path_for(user)
+    if user_signed_in?
+      flash[:notice] = 'You successfully logged in'
+      activities_path
+    else
+      super
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      new_user_session_path
+    else
+      root_path
+    end
+  end
+
 end
