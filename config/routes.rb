2ca4de1d68a3b5d1ce16dc2139b734776b113f67
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-18 11:08:32 +0200

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     resources :records
   end
   
-  resources :activities
+  resources :activities, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
