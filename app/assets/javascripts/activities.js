5027c9fcaae6c2945c84af8d3263399fe1c11ec1
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-02-19 11:40:35 +0200

    message

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
   var activityCards = document.querySelectorAll('.activity-card');
 
-  activityCards.forEach((card)=>{
-    card.addEventListener('mouseover', ()=>{
+  activityCards.forEach((card) => {
+    card.addEventListener('mouseover', () => {
       card.children[0].classList.toggle('d-none');  
       card.children[1].classList.toggle('d-none');  
     })
   });
   
-  activityCards.forEach((card)=>{
-    card.addEventListener('mouseout', ()=>{
+  activityCards.forEach((card) => {
+    card.addEventListener('mouseout', () => {
       card.children[0].classList.toggle('d-none');  
       card.children[1].classList.toggle('d-none');  
     })
